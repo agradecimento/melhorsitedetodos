c11153f791be22dd985b264cfe2adc4d9f787572
agradecimento <140530305+agradecimento@users.noreply.github.com>
2023-08-08 12:34:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+alert('eae')
+console.log("funcionando")
+  function PS22() {
+   console.log("playtaton 1")
+   document.getElementById("demo").innerHTML = "aqui está";
+   console.log("playtataton 2")
+   document.getElementById("demo2").src = "PS2.webp";
