72aaf00ad7633b340f2775d84839a0415e96d45f
agradecimento <140530305+agradecimento@users.noreply.github.com>
2023-08-15 12:31:23 -0300

    Add files via upload
    oogle

--- main2.js
+++ main2.js
@@ -1,7 +1,7 @@
 console.log("funcionando")
   function PS22() {
    console.log("playtaton 1")
-   document.getElementById("demo").innerHTML = "aqui está (clique no PS2 para uma supresa :DDDD)";
+   document.getElementById("demo").innerHTML = "aqui está";
    console.log("playtataton 2")
    document.getElementById("demo2").src = "PS2.webp";
    document.getElementById("demo2").alt = "Uma imagem de um PS2.";
